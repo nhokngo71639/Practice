3f28112065105937792dbd03ce72457f2f0062d1
nhokngo71639 <43365973+nhokngo71639@users.noreply.github.com>
2018-09-24 14:02:47 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
-class HelloWorld{  
+import java.util.Scanner;
+public class HelloWorld{  
   public static void main(String args[]){  
-   System.out.println("Hello World I am Quay");  
+    Scanner sc = new Scanner(System.in);
+        String name ;
+        System.out.print("Input name: ");
+        name = sc.nextLine();
+   System.out.println("Hello I Am" + " " + name);
     }  
 }  
