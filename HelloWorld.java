72794df3e9dc822bc0ca8b189fca82cf2a23aedb
nhokngo71639 <43365973+nhokngo71639@users.noreply.github.com>
2018-09-24 22:39:09 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws FileNotFoundException {
         int dem = 0;
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Input student list: ");
-        File file = new File(sc.nextLine());
+        File file = new File(scanner.nextLine());
         sc = new Scanner(file);
         System.out.println("Hello World This is TDTU");
         while(sc.hasNextLine()){
-            System.out.print("Hello World I am " + sc.nextLine()+ "\n");
+            System.out.print("Hello World I am " + scanner.nextLine()+ "\n");
             dem++;
         }
         System.out.print("Total " + count + " Students");
