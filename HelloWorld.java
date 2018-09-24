a0bd047f22db83f4f83977774527b895ec729b9a
nhokngo71639 <43365973+nhokngo71639@users.noreply.github.com>
2018-09-24 14:04:04 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{  
   public static void main(String args[]){  
-   System.out.println("Hello World I Am TDTU");  
+   System.out.println("Hello World This is TDTU");  
     }  
 }  
